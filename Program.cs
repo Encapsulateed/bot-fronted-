dfc5ead37919a6bce8f4c4a348c6d93fe13b2fd9
Encapsulateed <69109441+Encapsulateed@users.noreply.github.com>
2024-09-09 21:32:17 +0300

    base structure

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using DotNetEnv;
+
+namespace front_bot
+{
+   public partial class Program
+    {
+        private static ITelegramBotClient _bot;
+
+        private static ReceiverOptions _receiverOptions;
+
+        static async Task Main()
+        {
+            Env.Load("..//..//..//.env");
+
+            _bot = new TelegramBotClient(Environment.GetEnvironmentVariable("TOKEN")!);
+
+            _receiverOptions = new ReceiverOptions 
+            {
+                AllowedUpdates = new[]
+                {
+                    UpdateType.Message, UpdateType.CallbackQuery
+                },
+
+                ThrowPendingUpdates = true,
+            };
+
+            using var cts = new CancellationTokenSource();
+
+            _bot.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token); 
+
+            var me = await _bot.GetMeAsync(); 
+            Console.WriteLine($"{me.FirstName} запущен!");
+
+            await Task.Delay(-1);
+        }
+        private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try
+            {
+                switch (update.Type)
+                {
+                    case UpdateType.Message:
+                        {
+                            await HandleMessage(update.Message);
+                            return;
+                        }
+                    case UpdateType.CallbackQuery: 
+                        {
+                            await HandleCallBack(update.CallbackQuery);
+                            return;
+
+                        }
+                    default:
+                        throw new Exception("Unsupported update type!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = error switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => error.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/layer/MessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace front_bot
+{
+    public partial class Program
+    {
+        public static async Task HandleMessage(Message msg)
+        {
+            if (msg.Text.StartsWith('/')) HandleCommand();
+            else HandleUserMessage();
+        }
+
+        private static async Task HandleCommand() { }
+        private static async Task HandleUserMessage() { }
+    }
+}
